652e20c54efb12fe4e3fceff0ebdad22715a7d9c
Yoshimasa Niwa <niw@niw.at>
2008-11-03 22:16:30 +0000

    * update rb-gnome
    git-svn-id: svn+ssh://svn.dotswitch.net/var/svn/dotswitch/projects/library/ports@2133 c432a1ab-ec09-0410-8e5f-fb625be92370

--- ruby/rb-gtk2/files/patch-gtk-test-run-test.rb
+++ ruby/rb-gtk2/files/patch-gtk-test-run-test.rb
@@ -1,7 +1,7 @@
---- gtk/test/run-test.rb.orig	2008-06-06 23:03:41.000000000 +0900
-+++ gtk/test/run-test.rb	2008-07-11 23:51:33.000000000 +0900
-@@ -11,9 +11,9 @@
- test_unit_ext_dir = File.join(glib_base, "test-unit-ext", "lib")
+--- gtk/test/run-test.rb.orig	2008-06-14 20:36:00.000000000 +0900
++++ gtk/test/run-test.rb	2008-09-07 21:33:23.000000000 +0900
+@@ -13,9 +13,9 @@
+ require 'test/glib-test-init'
  
  [glib_base, atk_base, pango_base, gdk_pixbuf_base, gtk_base].each do |target|
 -  if system("which make > /dev/null")
